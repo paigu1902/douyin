ef9207f3401912163fea9566b6b1e50a74eb4328
吴星灿 <37405937+wuyoudexiao@users.noreply.github.com>
2023-01-12 13:50:23 +0000

    init

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
  "github.com/gin-gonic/gin"
+ "net/http"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.GET("/", func(context *gin.Context) {
+        context.String(http.StatusOK, "Hello World!")
+    })
+
 	r.Run()
 }
