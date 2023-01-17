7400d70a0654bf133cb4a17d5e786202589e411b
949696806 <949696806@qq.com>
2023-01-17 11:49:13 +0800

    jwt-go

--- controller/userLogin.go
+++ controller/userLogin.go
@@ -4,9 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"net/http"
+	"paigu1902/douyin/middlewares"
 	"paigu1902/douyin/models"
 	"paigu1902/douyin/utils"
 )
+
+type RegisterResponse struct {
+	StatusCode int64  `json:"status_code"` // 状态码，0-成功，其他值-失败
+	StatusMsg  string `json:"status_msg"`  // 返回状态描述
+	Token      string `json:"token"`       // 用户鉴权token
+	UserID     uint   `json:"user_id"`     // 用户id
+}
 
 func Register(c *gin.Context) {
 	username := c.Query("username")
@@ -19,11 +27,30 @@ func Register(c *gin.Context) {
 	if err == gorm.ErrRecordNotFound {
 		user.Password = utils.CalMD5(password)
 		models.DB.Create(&user)
-		c.JSON(http.StatusOK, "ok")
-	} else {
-		c.JSON(http.StatusOK, "no ok")
+		token, err := middlewares.GenerateToken(user.ID, user.UserName)
+		if err == nil {
+			response := RegisterResponse{
+				StatusCode: 0,
+				StatusMsg:  "注册成功",
+				Token:      token,
+				UserID:     user.ID,
+			}
+			c.JSON(http.StatusOK, response)
+		} else {
+			response := RegisterResponse{
+				StatusCode: 1,
+				StatusMsg:  "token生成失败",
+				Token:      token,
+				UserID:     user.ID,
+			}
+			c.JSON(http.StatusOK, response)
+		}
 	}
-
+	response := RegisterResponse{
+		StatusCode: -1,
+		StatusMsg:  "注册失败",
+	}
+	c.JSON(http.StatusOK, response)
 }
 
 //func Login(c *gin.Context) {
--- middlewares/auth.go
+++ middlewares/auth.go
@@ -3,6 +3,8 @@ package middlewares
 import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 var myKey = []byte("paigu1902")
@@ -43,4 +45,46 @@ func AnalyseToken(tokenString string) (*UserClaims, error) {
 		return nil, fmt.Errorf("analyse Token Error:%v", err)
 	}
 	return userClaim, nil
+}
+
+type Auth struct {
+	Token string `json:"token"`
+}
+
+func AuthUserCheck() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token, ok := c.GetQuery("token")
+		if !ok {
+			auth := new(Auth)
+			err := c.BindJSON(auth)
+			if err != nil {
+				c.Abort()
+				c.JSON(http.StatusOK, gin.H{
+					"code": http.StatusUnauthorized,
+					"msg":  "Unauthorized Authorization",
+				})
+				return
+			}
+			token = auth.Token
+		}
+		userClaim, err := AnalyseToken(token)
+		if err != nil {
+			c.Abort()
+			c.JSON(http.StatusOK, gin.H{
+				"code": http.StatusUnauthorized,
+				"msg":  "Unauthorized Authorization",
+			})
+			return
+		}
+		if userClaim == nil {
+			c.Abort()
+			c.JSON(http.StatusOK, gin.H{
+				"code": http.StatusUnauthorized,
+				"msg":  "Unauthorized Admin",
+			})
+			return
+		}
+		c.Set("user_claims", userClaim)
+		c.Next()
+	}
 }
