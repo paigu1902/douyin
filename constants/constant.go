f0ee6b10ba487c371a90c6a703e0c2d15b856d05
wqx <1761391865@qq.com>
2023-01-29 21:38:33 +0800

    视频上传

---
+++ constants/constant.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	VideoOperatorService = ""
+	bucketName           = "paigu-douyin-videos"
+	endpoint             = "oss-cn-nanjing.aliyuncs.com"
+	accessKeyId          = "LTAI5tKz8Jg67pok7LjZ3rus"
+	accessKeySecret      = "BUpR46wYSyON68Qpy7aGwEvCSCvzxf"
+)
