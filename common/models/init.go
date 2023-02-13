0d3d437e7fd9473bc9bccffccbb3053371b2953d
WuXingcan <949696806@qq.com>
2023-02-13 16:15:57 +0800

    一些修改

--- common/models/init.go
+++ common/models/init.go
@@ -9,11 +9,9 @@ import (
 
 var DB *gorm.DB
 
-//var RDB = InitRedisDB()
-
 func init() {
 	dsn := "root:123456@tcp(127.0.0.1:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{SkipDefaultTransaction: true})
 	if err != nil {
 		log.Println("gorm Init Error : ", err)
 		panic("fail to connect database")
@@ -34,11 +32,3 @@ func init() {
 		panic("fail to create table")
 	}
 }
-
-//func InitRedisDB() *redis.Client {
-//	return redis.NewClient(&redis.Options{
-//		Addr:     "localhost:6379",
-//		Password: "", // no password set
-//		DB:       0,  // use default DB
-//	})
-//}
