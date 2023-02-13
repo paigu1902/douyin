3031aafad66e13d1495f3de5d7aee034eed45b91
949696806 <949696806@qq.com>
2023-02-13 19:05:12 +0800

    引入nacos

--- service/rpc-user-relation/main.go
+++ service/rpc-user-relation/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	"github.com/kitex-contrib/registry-nacos/registry"
 	"log"
-	userRelationPb "paigu1902/douyin/service/rpc-user-relation/kitex_gen/userRelationPb/userrelation"
+	"net"
+	UserRelationPb "paigu1902/douyin/service/rpc-user-relation/kitex_gen/userRelationPb/userRelation"
 )
 
 func main() {
-	svr := userRelationPb.NewServer(new(UserRelationImpl))
-
-	err := svr.Run()
-
+	r, err := registry.NewDefaultNacosRegistry()
 	if err != nil {
-		log.Println(err.Error())
+		panic(err)
+	}
+	svr := UserRelationPb.NewServer(
+		new(UserRelationImpl),
+		server.WithServiceAddr(&net.TCPAddr{Port: 50052}),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: "userRelation"}),
+		server.WithRegistry(r),
+	)
+	if err := svr.Run(); err != nil {
+		log.Println("server stopped with error:", err)
+	} else {
+		log.Println("server stopped")
 	}
 }
