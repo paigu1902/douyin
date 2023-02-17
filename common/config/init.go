de2313211ffa509db633f110779acaada014e353
Ramsol <535848439@qq.com>
2023-02-17 22:53:02 +0800

    fix:修复config找不到路径问题

--- common/config/init.go
+++ common/config/init.go
@@ -1,14 +1,22 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"path"
+	"runtime"
+)
 
 var C *Config
 
 func init() {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		panic("配置初始化失败")
+	}
 	var configViperConfig = viper.New()
 	configViperConfig.SetConfigName("config")
 	configViperConfig.SetConfigType("yaml")
-	configViperConfig.AddConfigPath("../../../common/config/")
+	configViperConfig.AddConfigPath(path.Dir(filename))
 	//读取配置文件内容
 	if err := configViperConfig.ReadInConfig(); err != nil {
 		panic(err)
